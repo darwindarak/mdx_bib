b33135c679a95e170d94bc3f47d16c994353671f
Darwin Darakananda <darwindarak@gmail.com>
2015-07-13 09:11:45 -0700

    Initial commit
    This package provides RMarkdown style bibliography and citations support
    to Python-Markdown.  Currently, we can generating bibliography entries
    using either defined references or Bibtex entries in the same order that
    the citations appeared in the document.
    
    TODO:
    
    - Add tests and README
    - Implement option to alphabetize bibliography
    - Allow users to specify citation key formats
    - Allow users to define how the references are formatted (maybe using a
      CSL file?)

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mdx_bib',
+      description="A Python markdown extension for handling citations.",
+      version='0.0.1',
+      author = 'Darwin Darakananda',
+      py_modules=['mdx_bib'],
+      install_requires = ['markdown', 'pybtex'],
+      license='BSD')
